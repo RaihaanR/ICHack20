2568f83bf41179442b518b4b499e2f5c7f4814fa
Raihaan Rahman <raihaan2000@gmail.com>
2020-02-08 22:16:37 +0000

    Changed home page a bit

--- front-end/src/app/home/home.page.ts
+++ front-end/src/app/home/home.page.ts
@@ -7,6 +7,20 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public items: Array<{ title: string; note: string}> = [];
+
+  constructor() {
+    for (let i = 1; i < 5; i++) {
+
+      let current = new Date()
+      let time = current.getHours().toString() + ":"
+               + current.getMinutes().toString() + ":" 
+               + current.getSeconds().toString()
+      this.items.push({
+        title: 'Event ' + i,
+        note: time,
+      });
+    }
+  }
 
 }
