170abbb98a32c0944c4a3be69f0eaac89ef67338
Adi <agoel4512@gmail.com>
2020-02-08 22:09:57 +0000

    started buffer

--- back-end/index.js
+++ back-end/index.js
@@ -6,6 +6,13 @@ const port = 3000;
 
 app.use(cors());
 
+
+
+function hasFallen(timestampedData) {
+
+}
+
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/set/:param/:value', (req, res) => {
@@ -20,6 +27,26 @@ app.get('/settings', (req, res) => {
         res.send(JSON.parse(contents));
     });
 });
+
+app.get('/fallen', (req, res) => {
+    const objectSnapshot = '[{ "ts":1539055297728,\n' +
+        '     "objects":[\n' +
+        '          {\n' +
+        '               "frame":328,\n' +
+        '               "oid":13,\n' +
+        '               "x0":0.809,\n' +
+        '               "x1":0.696,\n' +
+        '               "y0":0.696,\n' +
+        '               "y1":0.356,\n' +
+        '               "type": "person"\n' +
+        '          }\n' +
+        '     ]' +
+        '}]';
+    const obj = JSON.parse(objectSnapshot);
+    let fallen = true
+    res.send(fallen)
+})
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
