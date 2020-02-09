86683191b265958fba5a93c3f1bcf22971373a5b
Adi <agoel4512@gmail.com>
2020-02-09 01:50:15 +0000

    Added twilio functionality - we say 999 not my phone number

--- back-end/index.js
+++ back-end/index.js
@@ -3,7 +3,13 @@ const fs = require('fs');
 const cors = require('cors');
 const app = express();
 const port = 3000;
+const accountSid = 'AC4a97a2dddc045ee98eb9e0f9fda08005';
+const authToken = '22655eaf3b6368233f1896e883c1447b';
+const client = require('twilio')(accountSid, authToken);
 
+
+let address = "36 Star Road \n" +
+              "W14 9XF \n"
 app.use(cors());
 
 app.get('/', (req, res) => res.send('Hello World!'))
@@ -25,6 +31,18 @@ app.get('/settings', (req, res) => {
 app.get('/fallen', (req, res) => {
     console.log("fallen");
     res.send("FALLEN")
+})
+
+app.get('/emergency', (req, res) => {
+    client.messages
+        .create({
+            body: 'There is an emergency at ' + address +
+                'Medical assistance is required. ' +
+                'Man of age 78 has collapsed. \n',
+            from: '+18133286624',
+            to: '+447414787312'
+        })
+        .then(message => console.log(message.sid));
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
