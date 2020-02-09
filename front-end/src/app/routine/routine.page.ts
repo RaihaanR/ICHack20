f6dab016eb34b543951effc5483b701c8e44e435
Raihaan Rahman <raihaan2000@gmail.com>
2020-02-09 00:34:28 +0000

    Added working saving routines

--- front-end/src/app/routine/routine.page.ts
+++ front-end/src/app/routine/routine.page.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
+import { ToastController } from '@ionic/angular';
 
 @Component({
     selector: 'app-list',
@@ -7,23 +9,96 @@ import {Component, OnInit} from '@angular/core';
 })
 export class RoutinePage implements OnInit {
     private alertTriggers = [];
-    detectFalls: boolean;
+
+    expressURL = 'http://localhost:3000';
+
     public actionOptions: Array<{ title: string; note: string }> = [{
         title: 'Notification',
         note: 'notify'
     }, {title: 'Call Default Phone', note: 'call'}, {title: 'Start video call', note: 'video'}];
 
-    public meals = [{title: 'Breakfast', selected: false}, {title: 'Lunch', selected: false}, {title: 'Dinner', selected: false}];
-    public bedtimes = [{title: 'Wake up', selected: false}, {title: 'Bed time', selected: false}];
-    public ranges = [{range: '15 minutes'}, {range: '30 minutes'}, {range: '1 hour'}];
+    public meals =
+    [{
+        title: 'Breakfast', note: 'breakfast', selected: false, time: ""
+    }, 
+    {
+        title: 'Lunch', note: 'lunch', selected: false, time: ""
+    },
+    {
+        title: 'Dinner', note: 'dinner', selected: false, time: ""
+    }];
 
-    constructor() {
+    public bedtimes = 
+    [{
+        title: 'Wake up', note: 'wake', selected: false
+    },
+    {
+        title: 'Bed time', note: 'bed', selected: false
+    }];
+
+    public ranges = 
+    [{
+        range: '15 minutes'
+    },
+    {
+        range: '30 minutes'
+    },
+    {
+        range: '1 hour'
+    }];
+
+    constructor(private http: HttpClient) {
+        this.http.get(this.expressURL + '/routine').subscribe(response => {
+            if (response.breakfast == "true") {
+                let meal = this.meals.find(x => x.note == "breakfast")
+                meal.selected = true
+                meal.time = response.breakfastTime
+            }
+
+            if (response.lunch == "true") {
+                let meal = this.meals.find(x => x.note == "lunch")
+                meal.selected = true
+                meal.time = response.lunchTime
+            }
+
+            if (response.dinner == "true") {
+                let meal = this.meals.find(x => x.note == "dinner")
+                meal.selected = true
+                meal.time = response.dinnerTime
+            }
+ 
+         });
     }
 
     ngOnInit() {
     }
 
     saveSettings() {
-        console.log(this.alertTriggers);
+        this.meals.forEach(x => {
+            console.log(x.note);
+
+            this.http.get(this.expressURL + '/routine/'+ x.note +'/' + x.selected).subscribe(response => {
+                console.log(response);
+            }); 
+
+            let time = document.getElementById(x.note + 'Time').value;
+            
+            this.http.get(this.expressURL + '/routine/'+ x.note + 'Time' +'/' + time).subscribe(response => {
+                console.log(response);
+            }); 
+        })
+
+        this.showSaved();
     }
+
+
+
+    async showSaved() {
+        const controller = new ToastController();
+        const toast = await controller.create({
+          message: 'Your settings have been saved.',
+          duration: 2000
+        });
+        toast.present();
+      }
 }
